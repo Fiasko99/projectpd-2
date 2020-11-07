eb4375165c15f46d7f9699071fe52d9808b0d2c5
Fiasko <snakeice2000@mail.ru>
2020-11-07 19:40:34 +0300

    f

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "std-mysql",
-    USER: "std_915",
-    PASSWORD: "rasulov01",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
     DB: "std_915",
     dialect: "mysql"
   };
